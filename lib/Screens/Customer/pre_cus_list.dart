af39555ea847d27143a8f01fb0d7812b0dc9853a
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2020-03-11 18:49:26 +0530

    pre cus list created

---
+++ lib/Screens/Customer/pre_cus_list.dart
@@ -0,0 +1,249 @@
+import 'dart:async';
+
+import 'package:autoassit/Screens/Vehicle/addVehicle.dart';
+import 'package:flutter/material.dart';
+import 'package:autoassit/Models/customerModel.dart';
+import 'package:autoassit/Controllers/ApiServices/Customer_Services/getCustomers_Service.dart';
+
+class PreCustomerList extends StatefulWidget {
+  PreCustomerList({Key key}) : super(key: key);
+
+  @override
+  _PreCustomerListState createState() => _PreCustomerListState();
+}
+
+class Debouncer {
+  final int milliseconds;
+  VoidCallback action;
+  Timer _timer;
+
+  Debouncer({this.milliseconds});
+
+  run(VoidCallback action) {
+    if (null != _timer) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+}
+
+class _PreCustomerListState extends State<PreCustomerList> {
+
+  final _debouncer = Debouncer(milliseconds: 500);
+  List<Customer> customer = List();
+  List<Customer> filteredCustomers = List();
+
+  // List _selectedIndexs = [];
+  final _search = TextEditingController();
+   bool isSearchFocused = false;
+
+
+     @override
+  void initState() {
+    super.initState();
+    GetCustomerService.getCustomers().then((customersFromServer) {
+      setState(() {
+        customer = customersFromServer;
+        filteredCustomers = customer;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false, // this avoids the overflow error
+      resizeToAvoidBottomInset: true,
+      appBar: _buildTopAppbar(context),
+      body:GestureDetector(
+        onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: _buildBody(context),
+
+      ) 
+      
+      
+    );
+  }
+
+   Widget _buildTopAppbar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(190.0),
+      child: Container(
+        color: Colors.transparent,
+        height: MediaQuery.of(context).size.height/0.5,
+        alignment: Alignment.center,
+        child: _buildStack(context),
+      ),
+    );
+  }
+
+  Widget _buildStack(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          child: Container(
+            height: MediaQuery.of(context).size.height - 420.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Color(0xFF8E8CD8),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(75.0),
+                  bottomRight: Radius.circular(75.0)),
+            ),
+          ),
+        ),
+        Positioned(
+            left: 10,
+            top: MediaQuery.of(context).size.height / 20.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Center(
+                    child: Image.asset(
+                  "assets/images/personas.png",
+                  width: 150,
+                  height: 100,
+                )),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Center(
+                    child: Text(
+                      'Customer   \n      List.. ',
+                      style: TextStyle(
+                          textBaseline: TextBaseline.alphabetic,
+                          fontFamily: 'Montserrat',
+                          fontSize: 25.0,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+        Positioned(
+            left: 20,
+            top: MediaQuery.of(context).size.height / 4.8,
+            child: Column(children: <Widget>[_buildSearchBar(context)]))
+      ],
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 30.0),
+      width: MediaQuery.of(context).size.width / 1.4,
+      height: 45,
+      // margin: EdgeInsets.only(top: 32),
+      padding: EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 2),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5)]),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        // controller: _search,
+        onTap: () {
+          setState(() {
+            isSearchFocused = true;
+          });
+        },
+        onChanged: (string) {
+          _debouncer.run(() {
+            setState(() {
+              filteredCustomers = customer
+                  .where((u) =>
+                      (u.fName.toLowerCase().contains(string.toLowerCase())))
+                  .toList();
+            });
+          });
+        },
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.grey,
+            size: 30,
+          ),
+          hintText: 'search',
+        ),
+      ),
+    );
+  }
+
+   Widget _buildBody(BuildContext context) {
+    return Center(
+          child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return Container(
+             child: _goToVehicleList(index,'assets/images/personas.png',filteredCustomers[index].fName +" "+ filteredCustomers[index].lName,filteredCustomers[index].cusid,filteredCustomers),
+          );
+        },
+        itemCount: filteredCustomers.length,
+      ),
+    );
+  }
+
+  Widget _goToVehicleList(index,String imgPath,cusName,phone,filteredCustomers) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+      child: GestureDetector(
+        onTap: () {
+          print("vehicle list opened");
+          print(filteredCustomers[index].cusid);
+          final customer_id = filteredCustomers[index].cusid;
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => AddVehicle(customer_id:customer_id)));
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                child: Row(children: [
+              Image.asset(imgPath,
+                   height: 60.0,
+                   width: 60.0,
+                  ),
+              SizedBox(width: 10.0),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(cusName,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(phone,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15.0,
+                        color: Colors.grey,
+                      )),
+                )
+              ])
+            ])),
+            IconButton(
+              icon: Icon(Icons.keyboard_arrow_right),
+              color: Colors.black,
+              onPressed: () {
+                print("vehicle list opened");
+                print(filteredCustomers[index].cusid);
+                final customer_id = filteredCustomers[index].cusid;
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => AddVehicle(customer_id:customer_id)));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
