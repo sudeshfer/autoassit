c5832d3c0681c90eaab6d0efab6fd0d41b9f79f6
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2020-03-11 15:18:46 +0530

    view customer form completed

---
+++ lib/Screens/Customer/customer_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomerList extends StatefulWidget {
+  CustomerList({Key key}) : super(key: key);
+
+  @override
+  _CustomerListState createState() => _CustomerListState();
+}
+
+class _CustomerListState extends State<CustomerList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Column(
+         children:<Widget>[
+           
+         ]
+       ),
+    );
+  }
+}
